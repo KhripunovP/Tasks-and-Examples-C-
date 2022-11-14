a79b8610072188303968ffbf21087dc77c65748b
Khripunov_P <dante-pash@mail.ru>
2022-11-15 02:14:02 +0300

    completed+

--- HW_task2(15.11.22)/Program.cs
+++ HW_task2(15.11.22)/Program.cs
@@ -3,6 +3,8 @@ Console.WriteLine("Введите два числа");
 int number1 = Convert.ToInt32 (Console.ReadLine());
 int number2 = Convert.ToInt32 (Console.ReadLine());
 int max = number1;
+int min = number2;
 if (number2 > number1)
-max = number2;
-Console.WriteLine($"максимальное из двух чисел {max}");
+{max = number2;
+min = number1;}
+Console.WriteLine($"максимальное из двух чисел - {max}, минимальное - {min}");
