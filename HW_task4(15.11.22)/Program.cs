ca6ae7c8396becfb9bfaaa4e2b00fd627ae624ac
Khripunov_P <dante-pash@mail.ru>
2022-11-15 00:54:31 +0300

    completed

--- HW_task4(15.11.22)/Program.cs
+++ HW_task4(15.11.22)/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите три числа");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+int number2 = Convert.ToInt32 (Console.ReadLine());
+int number3 = Convert.ToInt32 (Console.ReadLine());
+int max = number1;
+if (number2 > max)
+max = number2;
+if (number3 > max)
+max = number3;
+Console.WriteLine($"максимальное из трех чисел {max}");
