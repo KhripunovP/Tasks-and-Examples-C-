2dcd32c597ef4043ab7d71c1382c7a8c0d61aa18
Khripunov_P <dante-pash@mail.ru>
2022-11-15 00:37:19 +0300

    completed

--- HW_task2(15.11.22)/Program.cs
+++ HW_task2(15.11.22)/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите два числа");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+int number2 = Convert.ToInt32 (Console.ReadLine());
+int max = number1;
+if (number2 > number1)
+max = number2;
+Console.WriteLine($"максимальное из двух чисел {max}");
