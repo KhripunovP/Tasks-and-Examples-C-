de7cdb6b03bfeae6a3a387afdc30e1397f81ee25
Khripunov_P <dante-pash@mail.ru>
2022-11-15 01:11:41 +0300

    completed

--- HW_task6(15.11.22)/Program.cs
+++ HW_task6(15.11.22)/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+Console.WriteLine("Введенное число четное");
+else Console.WriteLine("Введенное число нечетное");
