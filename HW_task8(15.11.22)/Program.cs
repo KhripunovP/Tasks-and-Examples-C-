01e9b08fffbb4eb879f7757f03d03d1824f724a7
Khripunov_P <dante-pash@mail.ru>
2022-11-15 02:02:45 +0300

    completed

--- HW_task8(15.11.22)/Program.cs
+++ HW_task8(15.11.22)/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+Console.WriteLine("Четные числа в диапазоне от 1 до N:");
+if (n > 0)
+{
+while (i <= n)
+{
+if (i % 2 == 0)
+Console.WriteLine ($"{i}");
+i++;
+}}
+else
+{
+while (i >= n)
+{
+if (i % 2 == 0)
+Console.WriteLine ($"{i}");
+i--;
+}}
