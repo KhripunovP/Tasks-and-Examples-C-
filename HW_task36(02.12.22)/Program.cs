700fd9ac28145436a15b933a4e9243de1c416614
Khripunov_P <dante-pash@mail.ru>
2022-12-02 04:34:33 +0300

    completed

---
+++ HW_task36(02.12.22)/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < (array.Length - 1))
+            Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine($"]");
+}
+
+int OddsOfArray(int[] array)
+{
+    int sum = 0;
+for (int i = 0; i < array.Length; i++)
+    {
+        if (((i+1) % 2) != 0)
+        {
+            sum = sum + array[i];
+        }
+    }
+    return sum;
+}
+
+Console.WriteLine("введите количество чисел, минимально и максимально возможные числа");
+int n = Convert.ToInt32(Console.ReadLine());
+int min = Convert.ToInt32(Console.ReadLine());
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreateArrayRndInt(n, min, max);
+PrintArray(arr);
+int result = OddsOfArray(arr);
+
+Console.WriteLine($"Сумма нечетных чисел массива = {result}");
+
