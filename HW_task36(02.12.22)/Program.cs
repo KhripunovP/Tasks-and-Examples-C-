60112a950895ec1c95a106b9a11a1524526c92c1
Khripunov_P <dante-pash@mail.ru>
2022-12-04 19:35:33 +0300

    completed+

--- HW_task36(02.12.22)/Program.cs
+++ HW_task36(02.12.22)/Program.cs
@@ -29,12 +29,9 @@ void PrintArray(int[] array)
 int OddsOfArray(int[] array)
 {
     int sum = 0;
-for (int i = 0; i < array.Length; i++)
+    for (int i = 1; i < array.Length; i = i + 2)
     {
-        if ((i % 2) != 0)
-        {
-            sum = sum + array[i];
-        }
+        sum = sum + array[i];
     }
     return sum;
 }
